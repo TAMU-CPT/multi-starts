42d2844b89401ecd4cadd7ba78a73824370106ab
Eleni Mijalis <elenimijalis@gmail.com>
2017-05-05 15:53:36 -0500

    output to file

--- repair.py
+++ repair.py
@@ -7,6 +7,9 @@ from Bio.Data import CodonTable
 import sys
 
 def get_CDS_and_SD(feat):
+    """
+        return the CDS and SD feature in gene
+    """
     for sf in feat.sub_features:
         if sf.type == 'Shine_Dalgarno_sequence':
             sd = sf
@@ -18,40 +21,51 @@ def get_CDS_and_SD(feat):
     return cds, sd
 
 def break_start(seq):
+    """
+        change the start sequence while keeping the same
+        amino acid translation, if possible
+    """
     if seq == 'ATG':    # if methionine, no other option but ATG
         return 'ATG'
     elif seq == 'GTG':  # if valine, return GTA (still valine)
         return 'GTA'
     elif seq == 'TTG':  # if leucine, return TTA (still leucine)
         return 'TTA'
-    # table = CodonTable.unambiguous_dna_by_id[11]
 
 def break_sd(sd):
+    # table = CodonTable.unambiguous_dna_by_id[11]
     pass
 
 def repair(fasta, gff3):
     recs = {}
     for record in GFF.parse(gff3):
         recs[record.id] = record
+
+    seqs = []
     for seq in SeqIO.parse(fasta, 'fasta'):
         if seq.id not in recs:
             continue
 
-        if seq.id == 'GFP':
-            with open('test1.fa', 'w') as seqfile1:
-                SeqIO.write(seq, seqfile1, 'fasta')
-            current = recs[seq.id]
-            for feat in current.features:
-                cds, sd = get_CDS_and_SD(feat)
-                start = seq.seq[cds.location.start:cds.location.start+3]
-                broken_start = break_start(start)
-                if start != broken_start:
-                    seq.seq = seq.seq[0:cds.location.start] + broken_start + seq.seq[cds.location.start+3:]
-                else:
-                    break_sd(sd)
+        # if seq.id == 'GFP':
 
-            with open('test.fa', 'w') as seqfile:
-                SeqIO.write(seq, seqfile, 'fasta')
+            # with open('test1.fa', 'w') as seqfile1:
+                # SeqIO.write(seq, seqfile1, 'fasta')
+
+        current = recs[seq.id]
+        for feat in current.features:
+            cds, sd = get_CDS_and_SD(feat)
+            cds_start = seq.seq[cds.location.start:cds.location.start+3]
+            broken_start = break_start(cds_start)
+            if cds_start != broken_start:
+                # try to break start sequence while keeping amino acid the same
+                seq.seq = seq.seq[0:cds.location.start] + broken_start + seq.seq[cds.location.start+3:]
+            else:
+                # if couldn't change start, must break SD
+                break_sd(sd)
+            seqs.append(seq)
+
+    with open('out.fa', 'w') as seqfile:
+        SeqIO.write(seqs, seqfile, 'fasta')
 
 
 if __name__ == '__main__':
